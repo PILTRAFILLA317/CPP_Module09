596b9454c504756eba0d603599d6284986309784
Unai Martin marante <umartin-@c3r2s4.42urduliz.com>
2023-09-20 19:04:11 +0200

    ex01 main solved

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,6 +7,21 @@ int main(int argc, char **av) {
 	}
 	RPN rpn;
 	std::string input(av[1]);
+	std::stringstream stream(input);
+	std::string token;
+	int c = 0;
+	while (getline(stream, token, ' ')){
+		if (rpn.isNumber(token) == false && rpn.isOperator(token) == false){
+			std::cout << "Error: bad input" << std::endl;
+			return 1;
+		}
+		if (rpn.isNumber(token) == true)
+			c++;
+	}
+	if (c > 10){
+		std::cout << "Error: more than 10 numbers" << std::endl;
+		return 1;
+	}
 	rpn.calculate(input);
 	return 0;
 }
